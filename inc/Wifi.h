3509e86604bb85fe20a911aa73cc3aa23cac52be
Viktor Limishenko <dictor93@gmail.com>
2020-02-18 01:39:03 +0200

    Implemented protection against double include

--- inc/Wifi.h
+++ inc/Wifi.h
@@ -3,4 +3,17 @@
 #include "task.h"
 #include "queue.h"
 
+#ifndef _INC_WIFI_H_
+#define _INC_WIFI_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void Wifi_init();
+
+#ifdef __cplusplus
+}  // extern "C" 
+#endif 
+
+#endif
