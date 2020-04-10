0aff3fbd7b6a42c63c70b5a8e42f5eaf5e83bc11
andriivoronkin <andriivoronkin@gmail.com>
2020-04-11 01:17:56 +0300

    add config file for any config

---
+++ inc/Config.h
@@ -0,0 +1,16 @@
+
+#ifndef _INC_CONFIG_H_
+#define _INC_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SSID "Jesus"
+#define PWD "8715387153"
+
+#ifdef __cplusplus
+}  // extern "C" 
+#endif 
+
+#endif
